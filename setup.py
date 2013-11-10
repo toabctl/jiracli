bb164ff4d046e57f41a308785f31ce2652dac36d
Thomas Bechtold <thomasbechtold@jpberlin.de>
2013-11-10 17:32:51 +0100

    release: bump version to 0.2 for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.rst"), "r") as f:
 
 setup(
     name = "jiracli",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     scripts = ['jiracli'],
     package_data = {
