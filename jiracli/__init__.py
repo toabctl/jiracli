290321534a7c877503bff01e9099bdc0c6017170
Benoit Latinier <benoit.latinier@durable.com>
2014-11-03 17:12:24 +0100

    Add: give tabulate the maximum width of terminal

--- jiracli/__init__.py
+++ jiracli/__init__.py
@@ -36,6 +36,38 @@ log = None
 # path to the user configuration file
 user_config_path = os.path.expanduser('~/.jiracli.ini')
 
+# http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+def get_term_size():
+    env = os.environ
+
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl
+            import struct
+            import termios
+            cr = struct.unpack('hh',
+                               fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        # ## Use get(key[, default]) instead of a try/catch
+        # try:
+        #     cr = (env['LINES'], env['COLUMNS'])
+        # except:
+        #     cr = (25, 80)
+    return int(cr[1]), int(cr[0])
+
 
 def setup_logging(debug):
     global log
